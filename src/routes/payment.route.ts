50267beaf28ca0c3f270ba4015e0db3dfd867fba
Gabriel Ferreira Umbelino <gabriel.f.umbelino@gmail.com>
2025-05-03 01:10:22 -0300

    update routes

--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -3,10 +3,10 @@ import * as paymentRoutes from "../controllers/payment.controller";
 import { validateSchema } from "../middlewares/validation.middleware";
 import { createPaymentSchema, updatePaymentSchema } from "../schemas/payment.schema";
 
-const userRoute = Router();
+const paymentRoute = Router();
 
-userRoute.post("", validateSchema(createPaymentSchema), paymentRoutes.createPayment);
-userRoute.put("", validateSchema(updatePaymentSchema), paymentRoutes.updatePayment);
-userRoute.get("/:orderId", paymentRoutes.getPayment);
+paymentRoute.post("", validateSchema(createPaymentSchema), paymentRoutes.createPayment);
+paymentRoute.put("", validateSchema(updatePaymentSchema), paymentRoutes.updatePayment);
+paymentRoute.get("/:orderId", paymentRoutes.getPayment);
 
-export default userRoute;
+export default paymentRoute;
